92b1e3123a6a669b24168655f3b56a28edbcd2c9
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-12-11 17:50:50 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -1,2 +1,7 @@
 #include<stdio.h>
+#define TEMP_LOWER_LIMIT 0
+#define TEMP_UPPER_LIMIT 45
+#define SOC_LOWER_LIMIT 20
+#define SOC_UPPER_LIMIT 80
+#define CHARGERATE_UPPER_LIMIT 0.8
 int batteryIsOk(float temperature, float soc, float chargeRate)
