0d9243998cc21f35f86a7fb0432bd0d0e9d9785e
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-12-11 17:26:30 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -9,40 +9,44 @@
 
 int CheckTempRange(float temperature )
 {
-    
+   int  returnstatus =1;
     if(temperature < TEMP_LOWER_LIMIT || temperature > TEMP_UPPER_LIMIT) 
   {
     printf("Temperature out of range!\n");
-    
-    return 0;
+    returnstatus = 0;
   }
+   return returnstatus;
 }
 int CheckSocRange(float soc )
 {
+    int  returnstatus =1;
     if(soc < SOC_LOWER_LIMIT || soc > SOC_UPPER_LIMIT)
   {
     printf("State of Charge out of range!\n");
-    return 0;
+    returnstatus = 0;
   } 
+    return returnstatus;
 }
 int CheckchargeRate(float chargeRate )
 {
+    int  returnstatus =1;
     if(chargeRate > CHARGERATE_UPPER_LIMIT)
   {
     printf("Charge Rate out of range!\n");
-    return 0;
+    returnstatus = 0;
   } 
+   return returnstatus;
 }
 
 
 int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-    int ValidateFlag;
+    int returnstatus = 1;
     
-    ValidateFlag = CheckTempRange(temperature);
-    ValidateFlag = CheckSocRange(soc);
-    ValidateFlag = CheckchargeRate(chargeRate);
-    return 1;
+    returnstatus &= CheckTempRange(temperature);
+    returnstatus &= CheckSocRange(soc);
+    returnstatus &= CheckchargeRate(chargeRate);
+    return returnstatus;
 }
 
 int main() {
