413b63b5a640e52865b354d1c2506b00c2f5c563
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-12-20 17:13:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <assert.h>
-#include"checker.h"
+#include"BMS.h"
 
-int main() {
+int main()
+{
+  printf("Available Temperature Units \n 0: Celsius\n 1: Fahrenheit\n Default:Celsius\n");
+  //printf("Please Enter the Temperature Units corresponding Number:\n ");
+  //scanf("%d",&TempUnit);
+  TempUnit = 0;
+  printf("AVailable Languages for showing the warnings\n 0: ENGLISH\n 1: GERMAN\n Default:ENGLISH\n");
+  //printf("Please Enter the language corresponding Number:\n ");
+  //scanf("%d",&LookupTableWarningIndex);
+  LookupTableWarningIndex= 0;
+  CheckSelectedLanguage();
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
+//TESTCASE-2
+  TempUnit = 0;
+  LookupTableWarningIndex= 1;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(50, 40, 0.3));
+ //TESTCASE-3 
+  TempUnit = 1;
+  LookupTableWarningIndex= 1;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(104, 70, 0.7));
+  assert(batteryIsOk(104, 40, 0.3)); 
+  //TESTCASE-4 
+  TempUnit = 1;
+  LookupTableWarningIndex= 1;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(104, 80, 0.9));
+  //TESTCASE-5 
+  TempUnit = 1;
+  LookupTableWarningIndex= 0;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(104, 80, 0.9));
+  
+  return 0;
 }
