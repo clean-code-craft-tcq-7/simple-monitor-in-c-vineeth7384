488548c3a4ce43c2dfe3b2813ff7016ee7386c56
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-12-22 17:19:42 +0530

    Update main.c

--- main.c
+++ main.c
@@ -11,29 +11,29 @@ int main()
   printf("AVailable Languages for showing the warnings\n 0: ENGLISH\n 1: GERMAN\n Default:ENGLISH\n");
   //printf("Please Enter the language corresponding Number:\n ");
   //scanf("%d",&LookupTableWarningIndex);
-  LookupTableWarningIndex= 0;
+  ErrorMessageLookupTableIndex= 0;
   CheckSelectedLanguage();
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 //TESTCASE-2
   TempUnit = 0;
-  LookupTableWarningIndex= 1;
+  ErrorMessageLookupTableIndex= 1;
   CheckSelectedLanguage();
   assert(!batteryIsOk(50, 40, 0.3));
  //TESTCASE-3 
   TempUnit = 1;
-  LookupTableWarningIndex= 1;
+  ErrorMessageLookupTableIndex= 1;
   CheckSelectedLanguage();
   assert(batteryIsOk(104, 70, 0.7));
   assert(batteryIsOk(104, 40, 0.3)); 
   //TESTCASE-4 
   TempUnit = 1;
-  LookupTableWarningIndex= 1;
+  ErrorMessageLookupTableIndex= 1;
   CheckSelectedLanguage();
   assert(!batteryIsOk(104, 80, 0.9));
   //TESTCASE-5 
   TempUnit = 1;
-  LookupTableWarningIndex= 0;
+  ErrorMessageLookupTableIndex= 0;
   CheckSelectedLanguage();
   assert(!batteryIsOk(104, 80, 0.9));
   
