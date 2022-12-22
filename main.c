15819a48f6e9e4297eebe9b8362f87bcd419485b
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-12-22 17:45:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -36,6 +36,106 @@ int main()
   ErrorMessageLookupTableIndex= 0;
   CheckSelectedLanguage();
   assert(!batteryIsOk(104, 80, 0.9));
+  //TESTCASE-6
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(50, 40, 0.3));
+  //TESTCASE-7
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(40, 90, 0.3));
+  //TESTCASE-8
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(40, 70, 0.9));
+  //TESTCASE-9
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(0, 70, 0.3));
+  //TESTCASE-10
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 70, 0.3));
+  //TESTCASE-11
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 20, 0.3));
+  //TESTCASE-12
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 80, 0.3));
+  //TESTCASE-13
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 80, 0));
+  //TESTCASE-14
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 80, 0.7));
+  //TESTCASE-15
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 1;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(45, 80, 0.8));
+  //TESTCASE-16
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(50, 40, 0.3));
+  //TESTCASE-17
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(40, 90, 0.3));
+  //TESTCASE-18
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(40, 70, 0.9));
+  //TESTCASE-19
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(0, 70, 0.3));
+  //TESTCASE-20
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 70, 0.3));
+  //TESTCASE-21
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 20, 0.3));
+  //TESTCASE-22
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 80, 0.3));
+  //TESTCASE-23
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 80, 0));
+  //TESTCASE-24
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(batteryIsOk(45, 80, 0.7));
+  //TESTCASE-25
+  TempUnit = 0;
+  ErrorMessageLookupTableIndex= 0;
+  CheckSelectedLanguage();
+  assert(!batteryIsOk(45, 80, 0.8)); 
   
   return 0;
 }
